1f0e5eb2b4fb5b79cea2791a540c41f44b919d94
figobaek <66956480+figobaek@users.noreply.github.com>
2021-01-18 17:01:08 +0900

    Update number2answer.sql

--- number2answer.sql
+++ number2answer.sql
@@ -1,2 +1,6 @@
-select to_char(t.ts, 'YYYY-MM') AS month, count(distinct c.userid)  from raw_data.session_timestamp as t inner join raw_data.user_session_channel as c
-on t.sessionid = c.sessionid group by to_char(t.ts, 'YYYY-MM') order by 1 ;
+select to_char(t.ts, 'YYYY-MM') AS month, count(distinct c.userid)  
+from raw_data.session_timestamp as t 
+inner join raw_data.user_session_channel as c
+on t.sessionid = c.sessionid 
+group by to_char(t.ts, 'YYYY-MM') 
+order by 1 ;
